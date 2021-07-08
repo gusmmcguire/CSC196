8e0e83cb48635a08486c346c5a1802de94a6aa24
Gus <gusmcg1856@gmail.com>
2021-07-08 15:15:08 -0600

    end of class 7/7

--- Engine/Graphics/Shape.h
+++ Engine/Graphics/Shape.h
@@ -9,7 +9,7 @@ namespace gme {
 		Shape() {}
 		Shape(const std::vector<Vector2>& points, const Color& color) : points{ points }, color{ color }{}
 
-		void Draw(Core::Graphics& graphics, Vector2 position, float scale = 1);
+		void Draw(Core::Graphics& graphics, Vector2 position, float angle, float scale = 1);
 	private:
 		std::vector<Vector2> points;
 		Color color;
--- Game/Main.cpp
+++ Game/Main.cpp
@@ -10,12 +10,13 @@
 
 
 gme::Vector2 position{ 400,300 };
-std::vector<gme::Vector2> points = { {-5,-5}, {5,-5}, {0,5}, {-5,-5} };
-gme::Shape shape{ points, gme::Color{.79f,.23f,.90f} };
+std::vector<gme::Vector2> points = { {-5,-5}, {5,-5}, {0,10}, {-5,-5} };
+gme::Shape shape{ points, gme::Color{0 , 1 , 0 } };
 const float speed = 250;
 float timer = 0;
 gme::ParticleSystem particleSystem;
-
+gme::Vector2 psPosition;
+float angle = 0;
 
 float deltaTime;
 float gameTime = 0;
@@ -27,37 +28,41 @@ bool Update(float dt) {
 	bool quit = Core::Input::IsPressed(Core::Input::KEY_ESCAPE);
 	timer += dt * 5;
 
+	int x, y;
+	Core::Input::GetMousePos(x, y);
+	psPosition.x = static_cast<float>(x);
+	psPosition.y = static_cast<float>(y);
+	
 	if (Core::Input::IsPressed(Core::Input::BUTTON_LEFT)) {
-		int x, y;
-		Core::Input::GetMousePos(x, y);
-		gme::Vector2 psPosition;
-		psPosition.x = static_cast<float>(x);
-		psPosition.y = static_cast<float>(y);
+
 		std::vector<gme::Color> colors{ gme::Color::white, gme::Color::red, gme::Color::green,gme::Color::blue, gme::Color::purple, gme::Color::cyan,gme::Color::orange,gme::Color::yellow };
-		particleSystem.Create(psPosition, 100, 3, colors[gme::RandomRangeInt(0,colors.size())], 150);
+		particleSystem.Create(psPosition, 150, 2, colors[gme::RandomRangeInt(0,colors.size())], 150);
 	}
 
 	particleSystem.Update(dt);
 
-	gme::Vector2 input;
-	if (Core::Input::IsPressed('A') || Core::Input::IsPressed(Core::Input::KEY_LEFT)) {input.x = -1;}
-	if (Core::Input::IsPressed('D') || Core::Input::IsPressed(Core::Input::KEY_RIGHT)) { input.x = 1;}
-	if (Core::Input::IsPressed('W') || Core::Input::IsPressed(Core::Input::KEY_UP)) {input.y = -1;}
-	if (Core::Input::IsPressed('S') || Core::Input::IsPressed(Core::Input::KEY_DOWN)) {input.y = 1;}
-	position += input * speed * dt;
+	float thrust = 0;
+	if (Core::Input::IsPressed('A') || Core::Input::IsPressed(Core::Input::KEY_LEFT)) {angle -= 5 * dt;}
+	if (Core::Input::IsPressed('D') || Core::Input::IsPressed(Core::Input::KEY_RIGHT)) {angle += 5 * dt;}
+	if (Core::Input::IsPressed('W') || Core::Input::IsPressed(Core::Input::KEY_UP)) {thrust = speed;}
+	
+	//if (Core::Input::IsPressed('S') || Core::Input::IsPressed(Core::Input::KEY_DOWN)) {input.y = 1;}
+	position += gme::Vector2::Rotate(gme::Vector2::down, angle) * thrust * dt;
+	particleSystem.Create(position, 2, 0.5, gme::Color::white, 200);
 	return quit;
 }
 
 void Draw(Core::Graphics& graphics) {
-	//float scale = 1 + (std::sin(timer) + 1) * 2;
-	float scale = 3;
-	shape.Draw(graphics,position, scale);
+	float scale = 1 + (std::sin(timer) + 1) * 2;
+	shape.Draw(graphics, position, angle, 3);
 	particleSystem.Draw(graphics);
 
 	graphics.SetColor(gme::Color::white);
 	graphics.DrawString(10, 10, std::to_string(deltaTime).c_str());
 	graphics.DrawString(10, 25, std::to_string(gameTime).c_str());
 	graphics.DrawString(10, 40, std::to_string(1/deltaTime).c_str());
+
+	graphics.DrawString(10, 55, std::to_string(psPosition.Length()).c_str());
 }
 
 int main() {
@@ -65,9 +70,7 @@ int main() {
 	Core::Init(name, 800, 600, 60);
 	Core::RegisterUpdateFn(Update);
 	Core::RegisterDrawFn(Draw);
-
 	particleSystem.Startup();
-
 	Core::GameLoop();
 	Core::Shutdown();
 	particleSystem.Shutdown();
